d6b628a25e0afe23e572e98265ee9479fea19e10
Young <rhsiddlsidhd@nate.com>
2024-07-25 00:08:12 +0900

    fe login form complete

--- src/components/loginpage/Button.tsx
+++ src/components/loginpage/Button.tsx
@@ -3,13 +3,20 @@ interface buttonProps {
   customStyle?: string;
   children: ReactNode;
   type: "submit" | "reset" | "button";
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: React.FC<buttonProps> = ({ customStyle, type, children }) => {
+const Button: React.FC<buttonProps> = ({
+  customStyle,
+  type,
+  children,
+  onClick,
+}) => {
   return (
     <button
-      className={`${customStyle} mb-4 py-1 w-40 h-10   text-white border-2 hover:border-sky-500 rounded-2xl`}
+      className={`${customStyle} mb-4 py-1 w-40 h-10   text-white border-2 hover:border-sky-500 rounded-2xl valid:`}
       type={type}
+      onClick={onClick}
     >
       {children}
     </button>
